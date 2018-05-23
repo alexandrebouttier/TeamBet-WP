85bca4ab8a9b05c3409691008222be05b78db893
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-05-23 16:52:30 +0200

    add whychoose section

--- page-templates/home.php
+++ page-templates/home.php
@@ -18,6 +18,38 @@ get_header();
     </div>
 </div>
 <!-- // Fin jumbotron -->
+
+   <!-- Début Whychoose -->
+   <section id="whychoose">
+        <div class="container">
+            <h3 class="section-title"><?php the_field('whychoose_titre');?></h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="bloc">
+                    <img class="img-fluid" src="<?php the_field('raison_1_img');?>">
+                        <p><?php the_field('raison_1');?></p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="bloc">
+                    <img class="img-fluid" src="<?php the_field('raison_2_img');?>">
+                        <p><?php the_field('raison_2');?></p>
+                    </div>
+
+                </div>
+                <div class="col-md-4">
+                    <div class="bloc">
+                    <img class="img-fluid" src="<?php the_field('raison_3_img');?>">
+                        <p><?php the_field('raison_3');?></p>
+                    </div>
+                </div>
+                <a href="inscription" class="btn btn-success my-2 my-sm-0">INSCRIPTION</a>
+            </div>
+
+
+        </div>
+    </section>
+    <!-- // Fin Whychoose -->
 <?php
 get_footer();
 ?>
