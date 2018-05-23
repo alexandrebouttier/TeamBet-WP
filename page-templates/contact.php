bc332bc6b1937cc4f7538a48b35c50b2e3778dcc
Alexandre Bouttier <alexandrebouttier@gmail.com>
2018-05-23 23:41:41 +0200

    add contact page

---
+++ page-templates/contact.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Template Name: Contact
+ */
+
+get_header();
+
+?>
+
+<!-- Début contact-->
+<section id="contact">
+
+    <div class="container">
+
+        <h3 class="section-title"><?php the_title();?></h3>
+
+        <div class="row">
+
+          <div class="col-md-6 center">
+
+          <form>
+           <?php the_post(); the_content();?>
+          </form>
+
+      </div>
+
+        </div>
+
+    </div>
+
+</section>
+<!-- // Fin contact -->
+<?php
+get_footer();
+?>
+
